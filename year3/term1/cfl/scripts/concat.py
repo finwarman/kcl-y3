7834eedb7847908450b7ffcfc1dc09af4dff21e4
Fin <finleywarman@hotmail.co.uk>
2020-09-29 23:01:20 +0100

    Add CFL scripts folder with language concat helper

---
+++ year3/term1/cfl/scripts/concat.py
@@ -0,0 +1,30 @@
+# helper function to raise languages to the nth power
+# langpower(a, b, 2) can be used to concat two separate languages
+
+
+def lang_power(result, base, power):
+    if power <= 1:
+        return result
+    else:
+        new_result = set()
+        for a in result:
+            for b in base:
+                new_result.add(a + b)
+        print(len(new_result))
+        return lang_power(sorted(list(new_result)), base, power-1)
+
+#  Usage:
+
+# a = ['a', 'b', 'c', '']
+# result = lang_power(a, a, 4)
+
+# result = lang_power(
+#     ['a', 'b', 'c', 'd'],
+#     ['1', '2', '3', '4', '5', '6', '7'],
+#     2
+# )
+
+
+print('\n')
+print(result)
+print(len(result))
